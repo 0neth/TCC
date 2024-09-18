42e3b8f9ad38eec376a188024704532f38ac48db
Neth <69278687+0neth@users.noreply.github.com>
2024-09-18 12:22:15 -0300

    Add files via upload
    JANELADEPRODUTOS

--- JanelaProdutos.py
+++ JanelaProdutos.py
@@ -9,12 +9,17 @@ class JanelaProdutos(QMainWindow):
         self.setWindowTitle("Registrar produtos")
         self.setGeometry(30,30,700,600)
         self.nome_produto = QLabel("Nome",self)
-        self.preco = QLabel("preco",self)
-        self.peso = QLabel("peso",self)
-        self.validade = QLabel("peso",self)
+        self.nome_produto_line = QLineEdit(self)
+        self.preco = QLabel("Preco",self)
+        self.preco_line = QLineEdit(self)
+        self.peso = QLabel("Peso",self)
+        self.peso_line = QLineEdit(self)
+        self.validade = QLabel("Validade",self)
+        self.validade_line = QLineEdit(self)
         self.marca = QLabel("Marca",self)
+        self.marca_line = QLineEdit(self)
         #self.estoque = QLabel("Estoque",self)
-        self.nome_line = QLineEdit(self)
+        
         #self.codigo_barras = QLabel("Codigo de barras", self)
         self.botao_registrar = QPushButton("Registrar",self)
         self.initUI()
@@ -22,18 +27,25 @@ class JanelaProdutos(QMainWindow):
     def initUI(self):
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-        self.nome_produto.setFont(QFont("", 15))
-        self.nome_produto.setGeometry(0,0,20,20)
-        self.nome_line.setPlaceholderText("Insira o nome")
-        
+        self.nome_produto_line.setPlaceholderText("nome")
+        self.preco_line.setPlaceholderText("preco")
+        self.peso_line.setPlaceholderText("peso")
+        self.validade_line.setPlaceholderText("validade")
+        self.marca_line.setPlaceholderText("marca")
+
         GridLayout = QGridLayout()
         GridLayout.addWidget(self.nome_produto,0,0)
-        GridLayout.addWidget(self.nome_line,1,0)
+        GridLayout.addWidget(self.nome_produto_line,1,0)
         GridLayout.addWidget(self.preco,0,2)
+        GridLayout.addWidget(self.preco_line,1,2)
         GridLayout.addWidget(self.peso,0,3)
+        GridLayout.addWidget(self.peso_line,1,3)
         GridLayout.addWidget(self.validade,0,4)
+        GridLayout.addWidget(self.validade_line,1,4)
         GridLayout.addWidget(self.marca,0,5)
-        GridLayout.addWidget(self.botao_registrar,1,5)
+        GridLayout.addWidget(self.marca_line,1,5)
+
+        GridLayout.addWidget(self.botao_registrar,1,6)
         
         central_widget.setLayout(GridLayout)
 
@@ -41,16 +53,26 @@ class JanelaProdutos(QMainWindow):
             QLineEdit{
                 font-size: 20px;
                 font-family: Arial;
-                padding 15px 50px;
+                padding: 0px 25px;
                 margin: 5px;
                 border: 3px solid;
-                border-radius: 15px; 
+                border-radius: 15px;
+            }
+            QLabel{
+                font-size:25px;
+                font-family-Arial;
+                padding:  0px 15px;
+                ;
+                margin: 10px;
+                border: 1px solid;
             }
         """)
         
         self.botao_registrar.setGeometry(0,0,200,50)
         self.botao_registrar.clicked.connect(self.on_click)
 
+        
+        
     def on_click(self):
         print("button clicked")
         self.botao_registrar.setText("Registrando...")
